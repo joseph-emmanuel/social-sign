916b2771946f8d013eaadfa1923c04486dd7b6a7
joseph-emmanuel <joseph.emmanuel@outlook.com>
2020-05-17 15:14:25 -0400

    added featurd for login to account

---
+++ includes/login.inc.php
@@ -0,0 +1,62 @@
+<?php
+
+if (isset($_POST['login-submit'])) {
+
+    require 'dbh.inc.php';
+
+    $mailuid = $_POST['mailuid'];
+    $password = $_POST['pwd'];
+    if (empty($mailuid)||empty($password)){
+        header("location: ../index.php?error=emptyfields");
+        exit();
+    }
+    else{
+        $sql="select * from users where uidUsers=? or emailUsers=?;";
+        $stmt=mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt,$sql)){
+            header("location: ../index.php?error=sqlerror1");
+            exit();
+        }
+        else{
+            mysqli_stmt_bind_param($stmt,"ss",$mailuid,$mailuid);
+            mysqli_stmt_execute($stmt);
+            $result=  mysqli_stmt_get_result($stmt);
+            if ($row=mysqli_fetch_assoc($result))
+            {
+                $pwdcheck=password_verify($password,$row['pwdUsers']);
+                if($pwdcheck==false){
+                    header("location: ../index.php?error=wrongpwd");
+                    exit();
+                }
+                elseif ($pwdcheck==true){
+                    session_start();
+                    $_SESSION['userId']=$row['idUsers'];
+                    $_SESSION['userUId']=$row['uidUsers'];
+                    $_SESSION['userMail']=$row['emailUsers'];
+                    header("location: ../index.php?login=sucess");
+                    exit();
+                }
+                else{
+                    header("location: ../index.php?error=wrongpwd");
+                    exit();
+                }
+            }
+            else{
+                header("location: ../index.php?error=nouser");
+                exit();
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+}
+else{
+    header("location: ../index.php");
+    exit();
+}
--- login.php
+++ login.php
@@ -6,17 +6,17 @@ require "phps/header.php"
     <h1>Please enter your credentials</h1>
 </div>
 <!-- delete -->
-<form action="" method="post">
+<form action="includes/login.inc.php" method="post">
     <h2><span class="entypo-login"><i class="fa fa-sign-in"></i></span> Login</h2>
-    <button class="submit"><span class="entypo-lock"><i class="fa fa-lock"></i></span></button>
+    <button class="submit" name="login-submit"><span class="entypo-lock"><i class="fa fa-lock"></i></span></button>
     <span class="entypo-user inputUserIcon">
            <i class="fa fa-user"></i>
          </span>
-    <input type="text" class="user" placeholder="ursername"/>
+    <input type="text" name="mailuid" class="user" placeholder="ursername"/>
     <span class="entypo-key inputPassIcon">
            <i class="fa fa-key"></i>
          </span>
-    <input type="password" class="pass"placeholder="password"/>
+    <input type="password" name="pwd" class="pass"placeholder="password"/>
 </form>
 <!-- till here -->
 
